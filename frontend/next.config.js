88a668f857bea2882c0970412ddfb1453e507c06
RealMH-01 <leelee032791@gmail.com>
2026-04-25 21:08:12 +0800

    fix(frontend): proxy api requests through vercel rewrites

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://8.136.189.224/api/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
-
