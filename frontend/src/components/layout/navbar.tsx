8f09e026e7864c9fe8f7cacae7f0bf59d8f00de7
RealMH-01 <leelee032791@gmail.com>
2026-04-29 10:27:55 +0800

    adjust guest navbar position

--- frontend/src/components/layout/navbar.tsx
+++ frontend/src/components/layout/navbar.tsx
@@ -113,7 +113,7 @@ export function Navbar() {
         <nav
           className={cn(
             "hidden min-w-0 items-center justify-center gap-2 lg:flex xl:gap-3",
-            !isAuthenticated && "lg:translate-x-[29rem] xl:translate-x-[33rem] 2xl:translate-x-[37rem]"
+            !isAuthenticated && "lg:translate-x-[24rem] xl:translate-x-[28rem] 2xl:translate-x-[32rem]"
           )}
         >
           {visibleNavItems.map((item) => (
